cfa34dbfe0b1709919a26651a57fd589a65d481d
Vivek Sharma <vivek.codespace@gmail.com>
2026-03-14 18:36:47 +0530

    added ,

--- Backend/app.js
+++ Backend/app.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI {
+mongoose.connect(process.env.MONGODB_URI ,{
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
